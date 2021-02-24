d96a7b76195f61f45e7d1512bbd64c56f22b367b
dasuco <david.sucias@gft.com>
2021-02-24 13:34:59 +0100

    Create Utilities file

---
+++ uAlert/Supporting Files/Utilities.swift
@@ -0,0 +1,9 @@
+//
+//  Utilities.swift
+//  uAlert
+//
+//  Created by Sucias Colomer, David on 24/02/2021.
+//  Copyright © 2021 Sucias Colomer, David. All rights reserved.
+//
+
+import Foundation
